358a189522afceed525ffc2df3687d9af59e0917
MLStruckmann <magnus.struckmann@gmail.com>
2020-05-22 11:56:46 +0200

    Add data to config

--- evolution/config.py
+++ evolution/config.py
@@ -1,3 +1,13 @@
+from sklearn import datasets, model_selection
+
+# Data config
+data_dict = datasets.load_iris()
+X,y = data_dict["data"], data_dict["target"]
+data = model_selection.train_test_split(X, 
+                                        y, 
+                                        test_size=0.3,
+                                        random_state=0)
+
 # Experiment config
 evolution_steps = 5
 
