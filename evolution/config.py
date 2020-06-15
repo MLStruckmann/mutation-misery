d39a2309d3ae3a7ba8f975205a2fda50e253f80e
MLStruckmann <magnus.struckmann@gmail.com>
2020-06-15 21:03:29 +0200

    Bug fixes

--- evolution/config.py
+++ evolution/config.py
@@ -1,16 +1,16 @@
 from data_prep import data_prep
 
 # Evolution config
-n_population_start = 50
+n_population_start = 150
 evolution_steps = 3
 
 # Selection config
-survival_ratio = 0.4
-mating_best_ratio = 0.3
-mating_lucky_ratio = 0.1
-n_survival_min = 1
-n_mating_best_min = 2
-n_mating_lucky_min = 1
+survival_ratio = 0.9
+mating_best_ratio = 0.8
+mating_lucky_ratio = 0.15
+n_survival_min = 0
+n_mating_best_min = 0
+n_mating_lucky_min = 0
 selection_parameter = (survival_ratio,
                        mating_best_ratio,
                        mating_lucky_ratio,
@@ -22,7 +22,7 @@ selection_parameter = (survival_ratio,
 model_choice = 'model_nn'
 
 if model_choice == 'model_nn': 
-    neurons_l1 = [i for i in range(30,101)]
+    neurons_l1 = [i for i in range(30,131)]
     neurons_l2 = [i for i in range(10,71)]
     acitvation_func = ('relu',)
     model_parameter_space = (neurons_l1,
--- evolution/main.py
+++ evolution/main.py
@@ -7,6 +7,7 @@ def main():
     print("start algorithm")
     population = generate_population(config.model_parameter_space,config.n_population_start)
     data = config.data
+    
 
     for i in range(0,config.evolution_steps):
         member_fitness = train_population(population,data)
